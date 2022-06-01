7e65480cead568b7a9588e17addff45d1351f5a6
OjAntoni <zezyulin90@gmail.com>
2022-06-01 20:59:17 +0300

    removed const configuration from code

--- news-scheduler/src/main/java/by/whatson/mailscheduler/web/client/NewsClient.java
+++ news-scheduler/src/main/java/by/whatson/mailscheduler/web/client/NewsClient.java
@@ -5,7 +5,10 @@ import by.whatson.domain.Agency;
 import by.whatson.domain.Article;
 import by.whatson.mailscheduler.repository.AgencyRepository;
 import by.whatson.mailscheduler.web.dto.ArticlesResponseDto;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
@@ -13,6 +16,7 @@ import reactor.core.publisher.Mono;
 import java.util.List;
 
 @Component
+@Setter @Getter
 public class NewsClient {
     private AgencyRepository agencyRepository;
 
@@ -21,11 +25,16 @@ public class NewsClient {
         this.agencyRepository = agencyRepository;
     }
 
-    private final String URL="https://newsapi.org";
-    private final String AGENCIES_ENDPOINT="/v2/top-headlines/sources";
-    private final String API_KEY="a0556e8158344ab38db6c9176e92c15";
+    @Value("${url}")
+    private String URL;
+    @Value("${agencies.endpoint}")
+    private String AGENCIES_ENDPOINT;
+    @Value("${articles.endpoint}")
+    private String ARTICLES_ENDPOINT;
+    @Value("${api.key}")
+    private String API_KEY;
 
-    private String HTTP_REQUEST_FOR_NEWS="https://newsapi.org/v2/top-headlines?language=%s&pageSize=100&apiKey=%s";
+    private String HTTP_REQUEST_FOR_NEWS=URL+ARTICLES_ENDPOINT+"?language=%s&pageSize=100&apiKey=%s";
 
     public List<Agency> getAllAvailableAgencies(){
         return agencyRepository.findAll();
